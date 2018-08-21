cf6c70e3500ede0a7a3019b7ba66cc4ac2b32fdf
errware <ilanbuzzetti@gmail.com>
2018-08-21 18:42:37 -0500

    AbstractNode refactoring. Algorithm pattern w/ validity checks being predicated on MethodContext. Code should break now.

--- src/org/errware/tasktree/AbstractNode.java
+++ src/org/errware/tasktree/AbstractNode.java
@@ -9,4 +9,25 @@ public abstract class AbstractNode implements Node {
     public static void setContext(MethodContext _c){c = _c;}
     public static MethodContext getContext(){return c;}
     public void init(){}
+    
+    protected Predicate<MethodContext> validityPred;
+    protected Predicate<MethodContext> invalidityPred;
+    protected abstract void onValid();
+    protected abstract void onInvalid();
+    @Override
+    public final boolean isValid(TaskTree t){
+        if(validityPred.test(c)){
+            onValid();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public final boolean isInvalid(TaskTree t){
+        if(invalidityPred.test(c)){
+            onInvalid();
+            return true;
+        }
+        return false;
+    }
 }
